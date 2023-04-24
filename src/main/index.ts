7aac798ff69ade2dc8a3d57b856324077533beef
redhoodsu <redhoodsu@tencent.com>
2023-04-24 20:15:41 +0800

    feat: start server

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,4 +1,6 @@
 import { app, BrowserWindow } from 'electron'
+import { isDev, resolve } from './lib/util'
+import { execa } from 'execa'
 
 if (!app.requestSingleInstanceLock()) {
   app.quit()
@@ -12,12 +14,42 @@ async function createWindow() {
     title: 'VIVY',
   })
 
-  if (import.meta.env.MODE === 'development') {
+  if (isDev()) {
     win.loadURL('http://localhost:8080')
   }
 }
 
+async function startEasyDiffusion() {
+  const binPath = resolve('installer_files/env/bin')
+
+  const appDir = resolve('easy-diffusion/ui')
+  await execa(
+    'uvicorn',
+    [
+      'main:server_api',
+      '--app-dir',
+      appDir,
+      '--port',
+      '9000',
+      '--host',
+      '0.0.0.0',
+      '--log-level',
+      'error',
+    ],
+    {
+      cwd: appDir,
+      stdio: 'inherit',
+      env: {
+        PATH: `${binPath}:${process.env.PATH}`,
+        SD_UI_PATH: appDir,
+        PYTORCH_ENABLE_MPS_FALLBACK: '1',
+      },
+    }
+  )
+}
+
 app.on('ready', createWindow)
+app.on('ready', startEasyDiffusion)
 
 app.on('second-instance', () => {
   if (win) {
---
+++ src/main/lib/util.ts
@@ -0,0 +1,12 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+export function isDev() {
+  return import.meta.env.MODE === 'development'
+}
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+export function resolve(p) {
+  return path.resolve(__dirname, '../../', p)
+}
