9b727ee8495d9493411289a1e170ab1753e89979
redhoodsu <redhoodsu@tencent.com>
2023-04-26 20:07:50 +0800

    chore: small changes

--- src/renderer/components/sidebar/SideBar.tsx
+++ src/renderer/components/sidebar/SideBar.tsx
@@ -1,5 +1,14 @@
 import './Sidebar.scss'
 
 export default function () {
-  return <div id="sidebar">Sidebar</div>
+  return (
+    <div id="sidebar">
+      <div class="prompt">
+        <textarea />
+      </div>
+      <div className="negative-prompt">
+        <textarea />
+      </div>
+    </div>
+  )
 }
