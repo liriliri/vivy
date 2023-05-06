da79b0496a0ac3d359722582ff1660d054aea95e
redhoodsu <redhoodsu@tencent.com>
2023-05-06 11:48:59 +0800

    chore: small changes

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -3,6 +3,8 @@ import Gallery from './components/gallery/Gallery'
 import Toolbar from './components/toolbar/Toolbar'
 import Statusbar from './components/statusbar/Statusbar'
 import './App.scss'
+import 'luna-setting/luna-setting.css'
+import './luna.scss'
 
 export default function App() {
   return (
--- src/renderer/components/sidebar/SideBar.tsx
+++ src/renderer/components/sidebar/SideBar.tsx
@@ -1,15 +1,31 @@
+import { useRef, useEffect } from 'react'
+import LunaSetting from 'luna-setting'
 import './Sidebar.scss'
 
 export default function () {
+  const generateSettingRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const setting = new LunaSetting(
+      generateSettingRef.current as HTMLDivElement
+    )
+    setting.appendSelect('sampler', 'Euler', 'Sampler', '', {
+      Euler: 'euler',
+    })
+  }, [])
+
   return (
     <div id="sidebar">
-      <div className="prompt">
-        <textarea placeholder="Prompt" />
+      <div className="generate-basic">
+        <div className="prompt">
+          <textarea placeholder="Prompt" />
+        </div>
+        <div className="negative-prompt">
+          <textarea placeholder="Negative Prompt" />
+        </div>
+        <button className="generate-image button">Generate Image</button>
       </div>
-      <div className="negative-prompt">
-        <textarea placeholder="Negative Prompt" />
-      </div>
-      <button className="generate-image button">Generate Image</button>
+      <div ref={generateSettingRef} className="generate-setting"></div>
     </div>
   )
 }
