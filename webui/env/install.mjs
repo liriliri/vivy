40785edabf7b0b6f1707e5823c52a9c9925a4d0f
surunzi <surunzi@foxmail.com>
2023-05-24 00:19:58 +0800

    chore: small changes

--- webui/env/install.mjs
+++ webui/env/install.mjs
@@ -1,8 +1,12 @@
 #!/usr/bin/env zx
 import { installPackages } from './mamba.mjs'
-import { exportPath } from './util.mjs'
+import { exportPath, resolve } from './util.mjs'
 
 await installPackages(['python=3.10.6'])
 exportPath()
 
 await $`python -m pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple`
+
+const targetPath = resolve('stable-diffusion-webui')
+const pathPath = resolve('patch')
+await fs.copy(pathPath, targetPath)
