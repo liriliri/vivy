1f2e002975bc7876acbb03c7cc0ab7a8a85c24d8
redhoodsu <redhoodsu@tencent.com>
2023-06-21 11:32:17 +0800

    chore: small changes

--- webui/env/install.mjs
+++ webui/env/install.mjs
@@ -1,8 +1,8 @@
 #!/usr/bin/env zx
 import { installPackages } from './mamba.mjs'
-import { exportPath, resolve } from './util.mjs'
+import { exportPath } from './util.mjs'
 
 await installPackages(['python=3.10.6'])
 exportPath()
 
-await $`python -m pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple`
+await $`python -m pip config set global.index-url https://pypi.mirrors.ustc.edu.cn/simple`
