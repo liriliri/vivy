a5e0bed448da8ee6a78b9bc62f4d0f8ce43022e8
redhoodsu <redhoodsu@tencent.com>
2023-08-07 20:33:45 +0800

    feat: dark mode

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client'
 import { lazy } from 'react'
 import { isDev } from './lib/util'
 import hotKey from 'licia/hotkey'
+import isDarkMode from 'licia/isDarkMode'
 import getUrlParam from 'licia/getUrlParam'
 import './main.scss'
 import './icon.css'
@@ -34,4 +35,8 @@ ReactDOM.createRoot(container).render(<App />)
 
 if (isDev()) {
   hotKey.on('f5', () => location.reload())
+}
+
+if (isDarkMode()) {
+  document.body.classList.add('-theme-with-dark-background')
 }
