f29df06ee48c61d3ef594aed8b9f4ffa9f0432f6
redhoodsu <redhoodsu@tencent.com>
2023-10-13 16:26:18 +0800

    feat: support reading invokeai meta data

--- src/renderer/lib/genData.ts
+++ src/renderer/lib/genData.ts
@@ -11,6 +11,7 @@ import bytesToStr from 'licia/bytesToStr'
 import extract from 'png-chunks-extract'
 import replaceAll from 'licia/replaceAll'
 import exifr from 'exifr'
+import each from 'licia/each'
 
 interface IGenData {
   negativePrompt?: string
@@ -21,7 +22,7 @@ interface IGenData {
   model?: string
 }
 
-interface IClipboardGenData extends IGenData {
+interface ISDGenData extends IGenData {
   prompt: string
   width?: number
   height?: number
@@ -33,14 +34,51 @@ interface IImageGenData extends IGenData {
   height: number
 }
 
-const regParam = /\s*([\w ]+):\s*("(?:\\.|[^\\"])+"|[^,]*)(?:,|$)/g
+export function parseText(text: string): ISDGenData {
+  text = trim(text)
+  text = replaceAll(text, '\u0000', '')
 
-export function parseText(text: string): IClipboardGenData {
+  if (startWith(text, 'parameters')) {
+    return parseA1111(text)
+  }
+
+  if (startWith(text, 'sd-metadata')) {
+    return parseInvokeAI(text)
+  }
+
   return parseA1111(text)
 }
 
-export function parseA1111(text: string): IClipboardGenData {
-  text = trim(text)
+const regPrompt = /^(.*?)\[(.*?)\]$/
+
+function parseInvokeAI(text: string): ISDGenData {
+  text = trimStarts(text, ['sd-metadata'])
+  const { image } = JSON.parse(text)
+
+  let prompt = image.prompt
+  let negativePrompt = ''
+  const match = prompt.match(regPrompt)
+  if (match) {
+    prompt = trim(match[1])
+    negativePrompt = trim(match[2])
+  }
+
+  return {
+    prompt,
+    negativePrompt,
+    width: image.width,
+    height: image.height,
+    cfgScale: image.cfg_scale,
+    sampler: image.sampler,
+    seed: image.seed,
+    steps: image.steps,
+  }
+}
+
+const regParam = /\s*([\w ]+):\s*("(?:\\.|[^\\"])+"|[^,]*)(?:,|$)/g
+
+function parseA1111(text: string): ISDGenData {
+  text = trimStarts(text, ['parameters', 'UNICODE'])
   let prompt = ''
   let negativePrompt = ''
   let doneWithPrompt = false
@@ -74,12 +112,12 @@ export function parseA1111(text: string): IClipboardGenData {
     }
   }
 
-  const genData: IClipboardGenData = {
-    prompt,
+  const genData: ISDGenData = {
+    prompt: trim(prompt),
   }
 
   if (negativePrompt) {
-    genData.negativePrompt = negativePrompt
+    genData.negativePrompt = trim(negativePrompt)
   }
 
   if (matches) {
@@ -143,23 +181,29 @@ export async function parseImage(
     for (let i = 0, len = chunks.length; i < len; i++) {
       const chunk = chunks[i]
       if (startWith(chunk, 'parameters')) {
-        extend(genData, parseText(chunk.slice('parameters'.length)))
+        extend(genData, parseText(chunk))
         break
+      }
+      if (startWith(chunk, 'sd-metadata')) {
+        extend(genData, parseText(chunk))
       }
     }
   } else if (mime === 'image/jpeg') {
     const { userComment } = await exifr.parse(buf, ['UserComment'])
     if (userComment) {
-      extend(
-        genData,
-        parseText(
-          replaceAll(bytesToStr(userComment), '\u0000', '').slice(
-            'UNICODE'.length
-          )
-        )
-      )
+      extend(genData, parseText(bytesToStr(userComment)))
     }
   }
 
   return genData
+}
+
+function trimStarts(text, starts: string[]) {
+  each(starts, (start) => {
+    if (startWith(text, start)) {
+      text = text.slice(start.length)
+    }
+  })
+
+  return text
 }
