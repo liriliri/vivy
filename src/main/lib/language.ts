751c8a27b1254552492b214f0121ec62d9d9e655
redhoodsu <redhoodsu@tencent.com>
2024-04-05 23:47:48 +0800

    chore: small changes

--- src/main/lib/language.ts
+++ src/main/lib/language.ts
@@ -4,11 +4,9 @@ import zhCN from '../../common/langs/zh-CN.json'
 import I18n from 'licia/I18n'
 import defaults from 'licia/defaults'
 import types from 'licia/types'
-import { ipcMain } from 'electron'
+import { app, ipcMain } from 'electron'
 
 const store = getSettingsStore()
-const locale = Intl.DateTimeFormat().resolvedOptions().locale
-const systemLanguage = locale === 'zh-CN' ? 'zh-CN' : 'en-US'
 
 const i18n = new I18n('en-US', {
   'en-US': enUS,
@@ -19,13 +17,14 @@ export function t(path: string | string[], data?: types.PlainObj<any>) {
   return i18n.t(path, data)
 }
 
-let language = systemLanguage
+let language = 'en-US'
 export function get() {
   return language
 }
 
 export function init() {
   const lang = store.get('language')
+  const systemLanguage = app.getLocale() === 'zh-CN' ? 'zh-CN' : 'en-US'
   language = lang === 'system' ? systemLanguage : lang
   i18n.locale(language)
   ipcMain.handle('getLanguage', () => get())
