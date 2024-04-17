3bc2869dd55d2877ab9f4a80949c105bf8bc4797
redhoodsu <redhoodsu@tencent.com>
2024-04-18 00:43:10 +0800

    chore: remove useless files

--- script/pack.mjs
+++ script/pack.mjs
@@ -1,8 +1,25 @@
 import builder from 'electron-builder'
+import map from 'licia/map.js'
 
 cd('dist')
 
 const pkg = await fs.readJson('package.json')
+
+const ignoreSitePackages = map(
+  [
+    'aiohttp',
+    'fontTools',
+    'llvmlite',
+    'networkx',
+    'numba',
+    'pip',
+    'test',
+    'tzdata',
+  ],
+  (item) => {
+    return `!**/site-packages/{${item},${item}-*-info}`
+  }
+)
 
 const config = {
   directories: {
@@ -13,13 +30,17 @@ const config = {
     'preload',
     'renderer',
     'webui',
-    '!**/*.{js.map,md,MD}',
-    '!**/mamba',
+    '!**/*.{js.map,md,MD,a,c,h,cpp,pc,tcl}',
+    '!**/{.github,mamba}',
     '!**/taming-transformers/{data,assets}',
     '!**/stable-diffusion-stability-ai/assets',
+    '!**/env/{share,include,conda-meta,man,ssl}',
+    '!**/env/lib/tcl*',
     '!**/site-packages/**/__pycache__',
     '!**/site-packages/torch/include',
     '!**/site-packages/torch/lib/*.lib',
+    '!**/site-packages/pandas/tests',
+    ...ignoreSitePackages,
   ],
   asarUnpack: ['webui/**/*'],
   win: {
