6ffce02c8082670c188b43b24f5842bd8cee73f2
redhoodsu <redhoodsu@tencent.com>
2025-09-29 11:36:25 +0800

    chore: use share i18n

--- src/common/util.ts
+++ src/common/util.ts
@@ -1,20 +1,12 @@
-import I18n from 'licia/I18n'
 import defaults from 'licia/defaults'
-import types from 'licia/types'
 import enUS from './langs/en-US.json'
 import zhCN from './langs/zh-CN.json'
+import { init as initI18n } from 'share/common/i18n'
+export { t, i18n, hasLocale } from 'share/common/i18n'
 
 const langs = {
   'en-US': enUS,
   'zh-CN': defaults(zhCN, enUS),
 }
 
-export const i18n = new I18n('en-US', langs)
-
-export function hasLocale(locale: string) {
-  return !!langs[locale]
-}
-
-export function t(path: string | string[], data?: types.PlainObj<any>) {
-  return i18n.t(path, data)
-}
+initI18n(langs)
